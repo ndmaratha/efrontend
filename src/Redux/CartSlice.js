3cf241f087924fd75542e2083949be2369b24f25
Coder <ndmaratha01@gmail.com>
2024-09-06 12:49:09 +0530

    Update CartSlice.js

--- src/Redux/CartSlice.js
+++ src/Redux/CartSlice.js
@@ -42,7 +42,7 @@ export const {
 export const fetchCartData = () => async (dispatch) => {
   try {
     dispatch(fetchCartDataStart());
-    const res = await axios.get("https://ebackend-m32m.onrender.com/get/cart");
+    const res = await axios.get("https://ecommercebackend-cr2m.onrender.com/get/cart");
     dispatch(fetchCartDataSuccess(res.data));
   } catch (error) {
     dispatch(fetchCartDataFailure(error.message));
